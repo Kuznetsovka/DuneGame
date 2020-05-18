d21c8f4b172bf0bd0f19a50834a26be8139b32f3
Kirill Kuznetsov <kuznechik.86@mail.ru>
2020-05-19 00:38:57 +0300

    ДЗ 1

--- core/src/com/dune/game/DuneGame.java
+++ core/src/com/dune/game/DuneGame.java
@@ -2,55 +2,20 @@ package com.dune.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Vector2;
 
 public class DuneGame extends ApplicationAdapter {
-    private static class Tank {
-        private Vector2 position;
-        private Texture texture;
-        private float angle;
-        private float speed;
 
-        public Tank(float x, float y) {
-            this.position = new Vector2(x, y);
-            this.texture = new Texture("tank.png");
-            this.speed = 200.0f;
-        }
-
-        public void update(float dt) {
-            if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-                angle += 180.0f * dt;
-            }
-            if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-                angle -= 180.0f * dt;
-            }
-            if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-                position.x += speed * MathUtils.cosDeg(angle) * dt;
-                position.y += speed * MathUtils.sinDeg(angle) * dt;
-            }
-        }
-
-        public void render(SpriteBatch batch) {
-            batch.draw(texture, position.x - 40, position.y - 40, 40, 40, 80, 80, 1, 1, angle, 0, 0, 80, 80, false, false);
-        }
-
-        public void dispose() {
-            texture.dispose();
-        }
-    }
-
-    private SpriteBatch batch;
+    protected SpriteBatch batch;
     private Tank tank;
+    private Circle circle;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         tank = new Tank(200, 200);
+        circle = new Circle();
     }
 
     @Override
@@ -61,21 +26,33 @@ public class DuneGame extends ApplicationAdapter {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         tank.render(batch);
+        circle.render(batch);
         batch.end();
     }
 
     public void update(float dt) {
         tank.update(dt);
+        try {
+            if (isTouch())
+                circle.update();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean isTouch() {
+        return distance()<=tank.getHalfWidth() - tank.getOffset()+circle.getRadius();
+    }
+
+    private float distance() {
+        return (float) Math.sqrt( Math.pow(tank.getPosition().x - circle.getPosition().x,2) + Math.pow(tank.getPosition().y - circle.getPosition().y,2));
     }
 
     @Override
     public void dispose() {
         batch.dispose();
         tank.dispose();
+        circle.dispose();
     }
 
-    // Домашнее задание:
-    // - Задать координаты точки, и нарисовать в ней круг (любой круг, радиусом пикселей 50)
-    // - Если "танк" подъедет вплотную к кругу, то круг должен переместиться в случайную точку
-    // - * Нельзя давать танку заезжать за экран
 }
